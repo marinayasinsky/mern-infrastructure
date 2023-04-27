4b6d31d10747a11b6fef66cf8250b9222a0fe266
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-04-27 07:05:42 -0400

    SEI Cafe

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import NewOrderPage from './pages/NewOrderPage/NewOrderPage';
+import AuthPage from './pages/AuthPage/AuthPage';
+import OrderHistoryPage from './pages/OrderHistoryPage/OrderHistoryPage';
+import NavBar from './components/NavBar';
+
+import { getUser } from './utilities/users-service';
+
+import styles from './App.module.css';
+
+function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className={styles.App}>
+     { user ? 
+      <>
+      {/* <NavBar user={user} setUser={setUser}/> */}
+      <Routes>
+        <Route path='/orders/new' element={ <NewOrderPage user={user} setUser={setUser}/> }/>
+        <Route path='/orders' element={ <OrderHistoryPage user={user} setUser={setUser}/>  }/>
+        <Route path='/*' element={<Navigate to='/orders/new' />}/>
+      </Routes>
+      </>
+     : 
+      <AuthPage setUser={setUser}/>
+      }
+    </main>
+  );
+}
+
+export default App;
